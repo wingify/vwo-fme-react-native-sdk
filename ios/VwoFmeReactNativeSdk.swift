a4525c6e85ec737554dde40fe5896cd2221914fc
Parvesh Chauhan <parvesh.2595@gmail.com>
2025-07-30 10:45:09 +0530

    feat: added changes for health check

--- ios/VwoFmeReactNativeSdk.swift
+++ ios/VwoFmeReactNativeSdk.swift
@@ -112,7 +112,7 @@ class VwoFmeReactNativeSdk: RCTEventEmitter, IntegrationCallback, LogTransport {
       }
 
       var sdkName: String = "vwo-fme-react-native-sdk"
-      var sdkVersion: String = "1.7.3"
+      var sdkVersion: String = "1.7.4"
 
       let vwoOptions: VWOInitOptions
       vwoOptions = VWOInitOptions(sdkKey: sdkKey,
